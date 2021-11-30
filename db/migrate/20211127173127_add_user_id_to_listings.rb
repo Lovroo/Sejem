36f5a05b485ccefa87ef47ce91f3d2a77acc7076
lovro <lovro.napotnik@scv.si>
2021-11-30 12:53:13 +0100

    Migrating DB to Postgres

--- db/migrate/20211127173127_add_user_id_to_listings.rb
+++ db/migrate/20211127173127_add_user_id_to_listings.rb
@@ -1,5 +1,7 @@
 class AddUserIdToListings < ActiveRecord::Migration[6.1]
   def change
-    add_column :listings, :user_id, :reference
+    add_column :listings, :user_id, :integer
+    add_index :listings, :user_id
+    add_foreign_key :listings, :users
   end
 end
