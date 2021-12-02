20c3a692e7300028b5f1f69bd906e76191e244b7
lovro <lovro.napotnik@scv.si>
2021-12-02 08:36:59 +0100

    More refactoring of helpers

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,22 +1,22 @@
 module ListingsHelper
   def display_listing_links(listing)
+    concat content_tag(:td, link_to('Show' , listing))
     capture do
-    concat "<td>#{link_to 'Show', listing }</td>".html_safe
     if current_user == listing.user
-      concat "<td>#{link_to 'Edit', edit_listing_path(listing)}</td>".html_safe
-      concat "<td>#{link_to 'Destroy', listing, method: :delete, data: { confirm: 'Are you sure?' }} </td>".html_safe
+      concat content_tag(:td, link_to('Edit', edit_listing_path(listing)))
+      concat content_tag(:td, link_to('Destroy', listing, method: :delete, data: { confirm: 'Are you sure?' }))
     end
-    end
-    end
+        end
+  end
   def display_listing_show_links(listing)
     capture do
-    concat ""
-    if current_user == listing.user
-      concat link_to 'Edit', edit_listing_path(listing)
-      concat '|'
+      concat ' '
+        if current_user == listing.user
+         concat link_to 'Edit', edit_listing_path(listing)
+         concat ' '
+        end
+      concat link_to 'Back', listings_path
     end
-    concat link_to 'Back', listings_path
-    end
-    end
+  end
 end
 
