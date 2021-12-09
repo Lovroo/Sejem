fb51d990e7ab6705282119f40c077687ead92b33
lovro <lovro.napotnik@scv.si>
2021-12-09 09:09:07 +0100

    Edit profile link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def display_navbar_session_links
     capture do
       if user_signed_in?
+        concat link_to "Uredi Profil", edit_user_registration_path
+        concat " "
         concat link_to "Odjava", destroy_user_session_path, method: :delete
         concat " "
         concat link_to "Nov oglas", new_listing_path
