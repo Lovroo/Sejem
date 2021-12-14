be39685e26327736ade0479dd1c0a4265c79261d
lovro <lovro.napotnik@scv.si>
2021-12-14 11:55:16 +0100

    Fix migration

--- db/migrate/20211202080602_add_country_to_users.rb
+++ db/migrate/20211202080602_add_country_to_users.rb
@@ -2,5 +2,6 @@ class AddCountryToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :country, :string
     add_column :users, :state, :string
+    add_column :users, :city, :string
   end
 end
