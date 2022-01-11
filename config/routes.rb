5b485d0cb078efdb8ecac30debf44f709a951f17
lovro <lovro.napotnik@scv.si>
2022-01-11 12:02:06 +0100

    Login screen with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   get 'conversations/index'
   resources :categories
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :listings,
   controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
