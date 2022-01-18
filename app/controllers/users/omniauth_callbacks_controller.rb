5cd1080e035875a8934d2413c78fae2b16ec3c30
lovro <lovro.napotnik@scv.si>
2022-01-18 16:16:51 +0100

    Google API

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -12,48 +11,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-  def google_oauth2
-    @user = User.from_google(uid: auth.uid,
-                                  email: auth.info.email,
-                                  full_name: auth.info.name,
-                                  avatar_url: auth.info.image)
-    if @user.persisted?
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
-    else
-      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-      redirect_to new_user_registration_url
+    def google_oauth2
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      if @user.persisted?
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      end
     end
-  end
 
-  def failure
-    redirect_to root_path
-  end
-
-  protected
-
-  def after_omniauth_failure_path_for(_scope)
-    new_user_registration_url
-  end
-
-  def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
-  end
-  private
-
-  def from_google_params
-    @from_google_params ||= {
-      uid: auth.uid,
-      email: auth.info.email,
-      full_name: auth.info.name,
-      avatar_url: auth.info.image
-
-    }
-  end
-
-  def auth
-    @auth ||= request.env['omniauth.auth']
-  end
+    def failure
+      redirect_to root_path
+    end
 end
+
 
